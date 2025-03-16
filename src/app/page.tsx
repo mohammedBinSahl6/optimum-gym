39fdecace4ced0c863774d25687dd0295e68df64
omer mohamed <damouramory@gmail.com>
2025-03-16 17:31:50 +0200

    feat(intl): Integrate next-intl for internationalization support in layout and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-
+import { useTranslations } from "next-intl";
 export default function Home() {
+  const t = useTranslations("IndexPage");
   return (
     <div className="flex flex-col items-center justify-center gap-4 h-lvh">
+      {t("title")}
       <Image src="/assets/logo.svg" alt="Logo" width={128} height={128} />
     </div>
   );
