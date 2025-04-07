f9fc635e83bdcfc38447a1f2c5b1df10eed6d008
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-04-07 23:58:13 +0200

    feat(dashboard): Add getFilteredMembers function to retrieve members based on subscription status

---
+++ src/app/actions/getFilterdMembers.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { $Enums } from "@prisma/client";
+
+export type MembersBoard = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  image: string | null;
+  memberInfo: {
+    info: string | null;
+    userId: string;
+    height: number;
+    weight: number;
+    plan: string;
+    startDate: Date;
+    endDate: Date;
+    status: $Enums.SubscriptionStatus;
+    subscriptionCost: number;
+  } | null;
+}[];
+
+export async function getFilterdMembers(
+  filter: string
+): Promise<MembersBoard | "error"> {
+  try {
+    const members = await prisma.user.findMany({
+      where: {
+        role: "MEMBER",
+        memberInfo: {
+          status:
+            filter === "Active"
+              ? "ACTIVE"
+              : filter === "Inactive"
+              ? "UNACTIVE"
+              : filter === "Expired"
+              ? "EXPIRED"
+              : undefined,
+        },
+      },
+      select: {
+        memberInfo: true,
+        id: true,
+        image: true,
+        firstName: true,
+        lastName: true,
+      },
+    });
+
+    return members;
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+}
