caa7bc66bc29ed8a896fed66acd1c73f4019441e
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-04-15 15:47:18 +0200

    feat(ui): Add CoachPanel component for dashboard layout

---
+++ src/app/[locale]/(main)/dashboard/CoachPanel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoachPanel = () => {
+  return (
+    <div className='w-1/2 pt-10 flex flex-col gap-5 items-end'>
+      
+    </div>
+  )
+}
+
+export default CoachPanel
