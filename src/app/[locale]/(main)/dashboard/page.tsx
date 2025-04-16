c00468cc674787e6fdaad0b1002002d96614d1d2
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-04-16 21:25:18 +0200

    feat(dashboard): Enhance dashboard functionality with role-based panel rendering and user acceptance check

--- src/app/[locale]/(main)/dashboard/page.tsx
+++ src/app/[locale]/(main)/dashboard/page.tsx
@@ -1,16 +1,39 @@
-import { getCurrentUser } from "@/lib/session";
 import React from "react";
+
+import { getCurrentUser } from "@/lib/session";
 import AdminPanel from "./AdminPanel";
+import CoachPanel from "./CoachPanel";
 
 const DashboardPage = async () => {
   const user = await getCurrentUser();
 
+  const getDashboardPanel = () => {
+    switch (user?.role) {
+      case "ADMIN":
+        return <AdminPanel />;
+      // case "MEMBER":
+      //   return <MemberPanel />;
+      case "COACH":
+        return <CoachPanel user={user} />;
+      default:
+        return <div />;
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center py-10">
+    <div className="flex flex-col items-center justify-center py-10 gap-10">
       <h1 className="text-4xl font-bold text-primary-red">
         Hi {user?.firstName} {user?.lastName}
       </h1>
-      {user?.role === "ADMIN" ? <AdminPanel /> : <div />}
+      {user?.accepted ? (
+        getDashboardPanel()
+      ) : (
+        <div>
+          {" "}
+          Your request has sent, wait for admins to accept it to access your
+          dashboard
+        </div>
+      )}
     </div>
   );
 };
