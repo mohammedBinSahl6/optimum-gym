abb74615f81d90c8a613e4c9d90f3c06b1ddcebe
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-04-22 17:30:08 +0200

    feat(user): Implement getUserById action to retrieve user data by ID

---
+++ src/app/actions/getUserById.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export const getUserById = async (id: string) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
