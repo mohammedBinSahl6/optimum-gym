8cf00ab15d20206739f0b44b9900baab18f42ad2
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-04-30 22:35:23 +0300

    refactor(member): Extract user info retrieval logic into useEditPageUserInfo hook for cleaner EditMember component

--- src/app/[locale]/(main)/edit-member/[userId]/page.tsx
+++ src/app/[locale]/(main)/edit-member/[userId]/page.tsx
@@ -5,67 +5,22 @@ import { useParams } from "next/navigation";
 import { Edit2Icon } from "lucide-react";
 import { useSession } from "next-auth/react";
 
-import { MemberInfo, User } from "@prisma/client";
-import { getUserById } from "@/app/actions/getUserById";
 import Loader from "@/components/loader/Loader";
 import EditMemberInfoBoard from "../EditMemberInfoBoard";
 import EditMemberForm from "../EditMemberForm";
 import { cn } from "@/lib/utils";
-import { getMembershipsByUserId } from "@/app/actions/getMembershipsByUserId";
-import {
-  getPrivateSessionsByUserId,
-  PrivateSessionResponse,
-} from "@/app/actions/getPrivateSessions";
+import { PrivateSessionResponse } from "@/app/actions/getPrivateSessions";
+import useEditPageUserInfo from "@/hooks/useEditPageUserInfo";
 
 const EditMember = () => {
-  const [profile, setProfile] = React.useState<User>();
-  const [loading, setLoading] = React.useState(false);
   const [isEdit, setIsEdit] = React.useState(false);
-  const [memberships, setMemberships] = React.useState<MemberInfo[]>();
-  const [privateSessions, setPrivateSessions] =
-    React.useState<PrivateSessionResponse[]>();
+
+  React.useState<PrivateSessionResponse[]>();
 
   const { data } = useSession();
   const { userId } = useParams();
-
-  React.useEffect(() => {
-    const getProfile = async () => {
-      setLoading(true);
-      const response = await getUserById(userId as string);
-      if (response) {
-        setProfile(response);
-      }
-      setLoading(false);
-    };
-    getProfile();
-
-    const getMemberships = async () => {
-      setLoading(true);
-      const res = await getMembershipsByUserId(userId as string);
-
-      if (res) {
-        setMemberships(res);
-      } else {
-        setMemberships([]);
-      }
-      setLoading(false);
-    };
-    getMemberships();
-
-    const getPrivateSessions = async () => {
-      setLoading(true);
-      const res = await getPrivateSessionsByUserId(userId as string);
-
-      if (res) {
-        setPrivateSessions(res);
-      } else {
-        setPrivateSessions([]);
-      }
-      setLoading(false);
-    };
-
-    getPrivateSessions();
-  }, [userId]);
+  const { memberships, privateSessions, profile, loading } =
+    useEditPageUserInfo(userId as string);
 
   if (loading) {
     return (
