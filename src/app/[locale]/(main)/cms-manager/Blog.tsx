ae31cfafaec7f207a15ee73361aae18bbb6a0e2a
omer mohamed <damouramory@gmail.com>
2025-05-01 19:28:24 +0300

    feat(blog): Enhance Blog component with image support and navigation buttons; update BlogPosts to manage blog state and handle deletions

--- src/app/[locale]/(main)/cms-manager/Blog.tsx
+++ src/app/[locale]/(main)/cms-manager/Blog.tsx
@@ -2,39 +2,79 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
+import { useRouter } from "@/routes";
+
 export interface BlogProps {
   title: string;
   subtitle: string;
   content: string;
-  handleRemove?: () => void;
+  handleRemove?: (e: React.MouseEvent<SVGElement>) => void;
+  description?: string;
+  image?: string;
+  path: "dynamic" | "all";
 }
-const Blog = ({ content, subtitle, title, handleRemove }: BlogProps) => {
+
+const Blog = ({
+  content,
+  subtitle,
+  title,
+  handleRemove,
+  description,
+  image,
+  path,
+}: BlogProps) => {
   const [expand, setExpand] = useState(false);
 
-  function handleExpandContent() {
-    setExpand(!expand);
-  }
-  return (
-    <section className="flex flex-col border-primary-blue border-2 shadow-sm gap-8 w-full md:w-1/2  md:text-xl">
-      <X
-        onClick={handleRemove}
-        size={32}
-        className="cursor-pointer text-primary-blue hover:text-primary-red self-end "
-      />
-      <div className="flex flex-col gap-4 p-4">
-        <h1 className="text-4xl ">{title}</h1>
-        <h4>{subtitle}</h4>
-        <Button
-          className="self-start"
-          variant="ghost"
-          onClick={handleExpandContent}
-        >
-          {expand ? "Less" : "Read More"}
-        </Button>
-      </div>
+  const handleExpandContent = () => setExpand(!expand);
+  const router = useRouter();
 
-      {expand && <p className="p-8 text-primary-blue">{content}</p>}
-    </section>
+  const handleNavigateToPath = () => {
+    router.push(`/cms-manager/${description}`);
+  };
+
+  const handleNavigateToAll = () => {
+    router.push(`/cms-manager`);
+  };
+
+  return (
+    <>
+      <section className="flex flex-col border-primary-blue border-2 shadow-sm gap-8 w-full md:w-1/2 md:text-xl relative">
+        {image && (
+          <img
+            src={image}
+            alt="Blog post image"
+            className="w-full h-64  object-cover"
+          />
+        )}
+        <X
+          onClick={handleRemove}
+          size={32}
+          className="cursor-pointer text-primary-blue hover:text-primary-red absolute top-4 right-4"
+          data-id={description}
+        />
+        <div className="flex flex-col gap-4 p-4">
+          <h1 className="text-4xl">{title}</h1>
+          <h4>{subtitle}</h4>
+          <Button
+            className="self-start"
+            variant="ghost"
+            onClick={handleExpandContent}
+          >
+            {expand ? "Less" : "Read More"}
+          </Button>
+        </div>
+        {expand && <p className="p-8 text-primary-blue">{content}</p>}
+      </section>
+      {path == "all" ? (
+        <Button variant="link" onClick={() => handleNavigateToPath()}>
+          Navigate to Blog
+        </Button>
+      ) : (
+        <Button variant="link" onClick={() => handleNavigateToAll()}>
+          Return to all blogs
+        </Button>
+      )}
+    </>
   );
 };
 
--- src/app/[locale]/(main)/cms-manager/BlogPosts.tsx
+++ src/app/[locale]/(main)/cms-manager/BlogPosts.tsx
@@ -1,27 +1,24 @@
 "use client";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { Session } from "next-auth";
-
 import { useParams } from "next/navigation";
-
 import { toast } from "sonner";
 
 import { createBlog } from "@/scripts/createBlog";
 
-import { Blog as BlogType } from "@prisma/client";
-
-import Blog, { BlogProps } from "./Blog";
-
 import { getPathname } from "@/routes";
 import BlogDrawer from "./BlogDrawer";
+import Blog, { BlogProps } from "./Blog";
+
 import Loader from "@/components/loader/Loader";
 import { Button } from "@/components/ui/button";
 
+import { deleteAndRedirect } from "@/app/actions/removeBlog";
+
 const BlogPosts = ({ Blogs }: { Blogs: BlogProps[] }) => {
   const [loading, setLoading] = useState(false);
+  const [blogs, setBlogs] = useState<BlogProps[]>(Blogs);
   const { data, update, status } = useSession();
-  const [blogs, setBlogs] = useState<BlogProps[]>(Blogs); // initialize with server-side blogs
 
   const params = useParams();
   const locale = params.locale as string;
@@ -31,12 +28,13 @@ const BlogPosts = ({ Blogs }: { Blogs: BlogProps[] }) => {
     locale: locale,
   });
 
-  console.log(window.location);
   switch (status) {
     case "unauthenticated":
       return <h1 className="text-center">You are not authenticated</h1>;
+
     case "loading":
       return <Loader size="lg" />;
+
     case "authenticated":
       if (data.user.role === "ADMIN") {
         return (
@@ -50,35 +48,64 @@ const BlogPosts = ({ Blogs }: { Blogs: BlogProps[] }) => {
                   title: e.title,
                   email: data.user.email,
                 });
-                if (result.success) {
+
+                if (result.success && result.blog) {
                   toast.success("Blog created successfully");
-                  setBlogs((prevBlogs) => [...prevBlogs, result.blog]);
+
+                  setBlogs((prev) => [
+                    ...prev,
+                    {
+                      ...result.blog,
+                      path: "all",
+                      handleRemove: async (e) => {
+                        const id = e.currentTarget.dataset.id;
+                        if (!id) return;
+
+                        setBlogs((prev) =>
+                          prev.filter((b) => b.description !== id)
+                        );
+                        await deleteAndRedirect(id, navigate);
+                      },
+                    },
+                  ]);
                   await update();
                 } else {
                   toast.error("Failed to create blog");
                 }
+
                 setLoading(false);
               }}
               user={data.user}
               key={data.user.id}
             />
-            {Blogs.map((blog) => {
-              const encryptedKey = crypto.randomUUID();
 
-              return <Blog key={encryptedKey} {...blog} />;
-            })}
+            {blogs.map((blog) => (
+              <Blog
+                key={blog.description}
+                description={blog.description}
+                path="all"
+                handleRemove={async (e) => {
+                  const id = e.currentTarget.dataset.id;
+                  setBlogs((prev) => prev.filter((b) => b.description !== id));
+                  await deleteAndRedirect(id, navigate);
+                }}
+                data-id={blog.description}
+                {...blog}
+              />
+            ))}
           </section>
         );
       }
+
     default:
       return (
-        <div className="flex flex-col absolute top-1/2 left-1/2 ">
-          <h1 className="text-center self-center text-primary-red font-extrabold text-3xl  transform -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col absolute top-1/2 left-1/2">
+          <h1 className="text-center self-center text-primary-red font-extrabold text-3xl transform -translate-x-1/2 -translate-y-1/2">
             You are not an Admin
           </h1>
           <Button
             variant="blue"
-            className=" p-4  rounded-md self-start mt-4"
+            className="p-4 rounded-md self-start mt-4"
             onClick={() => (window.location.href = navigate)}
           >
             Back
@@ -87,4 +114,5 @@ const BlogPosts = ({ Blogs }: { Blogs: BlogProps[] }) => {
       );
   }
 };
+
 export default BlogPosts;
