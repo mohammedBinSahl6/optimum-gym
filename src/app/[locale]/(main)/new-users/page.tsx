d43310dab6d671d9065e993355e9e9e58a79104f
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-05-06 23:19:46 +0300

    feat(localization): Enhance MembershipDrawer and UserCard components with localization support for user interface elements

--- src/app/[locale]/(main)/new-users/page.tsx
+++ src/app/[locale]/(main)/new-users/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getTranslations } from "next-intl/server";
 
 import UserCard from "./UserCard";
 
@@ -9,10 +10,12 @@ const NewUsersPage = async () => {
     },
   });
 
+  const t = await getTranslations("NewUsersPage");
+
   return (
     <div className="flex flex-col items-center py-10 min-h-screen">
       <div className="w-1/2 flex flex-col gap-5 items-center bg-primary-light p-10 rounded-lg">
-        <h1 className="text-4xl font-bold text-primary-blue">New Users</h1>
+        <h1 className="text-4xl font-bold text-primary-blue">{t("Title")}</h1>
         {users.map((user) => (
           <UserCard user={user} key={user.id} />
         ))}
