dacffc7821c4c05feea0bfba781cf1dff24ad1fb
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-05-14 22:32:36 +0300

    refactor(ai-coach): Simplify placeholder handling by consolidating coach prompts into a single string and update loading state display

--- src/app/[locale]/(main)/ai-coach/page.tsx
+++ src/app/[locale]/(main)/ai-coach/page.tsx
@@ -18,27 +18,11 @@ const AICoachPage = () => {
   const [output, setOutput] = React.useState("");
   const [loading, setLoading] = React.useState(false);
 
-  const { data } = useSession();
+  const { data, status } = useSession();
 
   const t = useTranslations("AICoachPage");
 
-  const randomPlaceholder = React.useMemo(() => {
-    const placeholder = [
-      t("CoachPlaceholders.0"),
-      t("CoachPlaceholders.1"),
-      t("CoachPlaceholders.2"),
-      t("CoachPlaceholders.3"),
-      t("CoachPlaceholders.4"),
-      t("CoachPlaceholders.5"),
-      t("CoachPlaceholders.6"),
-      t("CoachPlaceholders.7"),
-      t("CoachPlaceholders.8"),
-    ];
-    return placeholder[Math.floor(Math.random() * 8)];
-  }, [t]);
-
   const typedOutput = useTypewriter(output, 1); // Add this line
-
 
   const handleSubmit = async () => {
     setLoading(true);
@@ -66,13 +50,19 @@ const AICoachPage = () => {
         OPtic Coach
       </h1>
       <h2 className="text-7xl font-black bg-gradient-to-r from-[#011936] to-[#a41623] text-transparent bg-clip-text">
-        {`${t("Greeting")} ${data?.user.firstName}!`}
+        {status === "loading" ? (
+          <Loader size="lg" />
+        ) : (
+          `${t("Greeting")} ${data?.user.firstName}!`
+        )}
       </h2>{" "}
       <div className="w-full max-w-3xl min-h-[10vh] p-10">
         {loading ? (
           <Loader size="lg" />
         ) : (
-          <ReactMarkdown remarkPlugins={[remarkGfm]}>{typedOutput || ''}</ReactMarkdown>
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {typedOutput || ""}
+          </ReactMarkdown>
         )}
       </div>
       <section className="w-full fixed bottom-0 right-0 flex justify-center items-center p-10">
@@ -81,7 +71,7 @@ const AICoachPage = () => {
             rows={2}
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
-            placeholder={randomPlaceholder}
+            placeholder={t("CoachPlaceholder")}
             className="w-full"
           />
           <Button onClick={handleSubmit} disabled={!prompt.trim() || loading}>
