ce052a5491144216e9053c87de2bf553e5d52173
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-05-15 02:44:31 +0300

    feat(coaching): Add CoachCard component to display coach information with image and profile link

---
+++ src/app/[locale]/(main)/coaching/CoachCard.tsx
@@ -0,0 +1,29 @@
+import { User } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+
+interface CoachCardProps {
+  coach: User;
+}
+
+const CoachCard = ({ coach }: CoachCardProps) => {
+  return (
+    <div className="max-w-md w-full bg-primary-red rounded-3xl p-5 flex gap-5 items-center">
+      <Image
+        src={coach.image ?? "/assets/no-pic.svg"}
+        width={90}
+        height={90}
+        alt={coach.firstName}
+        className="rounded-3xl"
+      />
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl font-bold text-white">
+          C: {coach.firstName} {coach.lastName}
+        </h1>
+        <span className="text-sm text-white mt-auto">Show profile</span>
+      </div>
+    </div>
+  );
+};
+
+export default CoachCard;
