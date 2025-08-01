9c0d6e59fd242f9185bbad6eefbb09d5d228f162
ObadaFadloun <obada.fadloun@gmail.com>
2025-08-02 02:20:20 +0300

    refactor: Remove unused translation hook from Home component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
-import { useTranslations } from "next-intl";
 import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
-  const t = useTranslations("IndexPage");
   return (
     <div className="flex flex-col items-center justify-center h-lvh">
       <Hero />
