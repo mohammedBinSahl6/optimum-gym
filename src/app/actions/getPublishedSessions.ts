57c014558ff41b6ff95ae6fcfa646d60c453db59
mohammed_binsahl <mohammed.dbs.660@gmail.com>
2025-08-23 13:53:43 +0300

    feat(Sessions): Add function to retrieve published sessions from the database

---
+++ src/app/actions/getPublishedSessions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export async function getPublishedSessions() {
+  try {
+    const sessions = await prisma.session.findMany({
+      where: {
+        published: true,
+      },
+    });
+    return sessions;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
